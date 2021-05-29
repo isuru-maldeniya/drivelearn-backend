263cf7ab8ee7f0995c06ff18cd1f893097e8d57c
Isuru-Maldeniya <isurumaldeniya@gmail.com>
2021-05-29 16:46:10 +0530

    Initial commit

---
+++ src/main/java/com/drivelearn/drivelearnbackend/DrivelearnbackendApplication.java
@@ -0,0 +1,13 @@
+package com.drivelearn.drivelearnbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivelearnbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrivelearnbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/drivelearn/drivelearnbackend/DrivelearnbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drivelearn.drivelearnbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivelearnbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
